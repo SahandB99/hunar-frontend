26cf56e128e25440e9fe33d2b4cdcf293460f13d
SahandB99 <sahandb25@gmail.com>
2023-04-30 15:58:33 +0300

    fixed price error NaN in product page

--- src/pages/articles/Product.jsx
+++ src/pages/articles/Product.jsx
@@ -2,18 +2,18 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Container from "../../components/container/Container";
 import "./Product.style.css";
-import { data } from "../../data";
+import { products } from "../../products.js";
 import { useCart } from "react-use-cart";
 
 const Product = () => {
   const { addItem } = useCart();
-  const [article, setArticle] = useState();
+  const [product, setProduct] = useState();
   const { id } = useParams();
 
   useEffect(() => {
-    for (const d of data) {
-      if (d.id.toString() === id) {
-        setArticle(d);
+    for (const product of products) {
+      if (product.id.toString() === id) {
+        setProduct(product);
       }
     }
   }, []);
@@ -21,16 +21,16 @@ const Product = () => {
   return (
     <Container>
       <article className="product">
-        {article && (
+        {product && (
           <>
-            <h1 className="product-title">{article.title}</h1>
+            <h1 className="product-title">{product.title}</h1>
             <figure>
-              <img src={article.imgUrl} alt="news" className="product-image" />
+              <img src={product.imgUrl} alt="news" className="product-image" />
             </figure>
-            <p className="product-description">{article.desc}</p>
-            <p className="product-price">{article.price}</p>
+            <p className="product-description">{product.desc}</p>
+            <p className="product-price">{product.price}</p>
             <button
-                onClick={() => addItem(article)}
+                onClick={() => addItem(product)}
                 className="product-catalog__item-button"
               >
                 Add to Cart
